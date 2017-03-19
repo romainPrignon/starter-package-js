f72519e0233cfb33bc75467d1f38754f2502be8f
Romain Prignon <pro.rprignon@gmail.com>
2017-03-19 23:12:09 +0100

    feat: more es6 features

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import {Prime} from './prime'
+import {sortUserByAge} from './user'
+
+export {
+  Prime,
+  sortUserByAge
+}
---
+++ src/user.js
@@ -0,0 +1,29 @@
+const users = [
+  {
+    name: 'Martin',
+    age: 14,
+    orders: [
+      {name: 'ham', price: 2, quantity: 2},
+      {name: 'eggs', price: 1, quantity: 6},
+      {name: 'bacon', price: 5, quantity: 1},
+    ]
+  },
+  {
+    name: 'Elizabeth',
+    age: 36,
+    hair: 'black',
+    orders: [
+      {name: 'ham', price: 2, quantity: 2},
+      {name: 'bread', price: 1, quantity: 3},
+      {name: 'apples', price: 2, quantity: 3},
+    ]
+  }
+]
+
+export const sortUserByAge = () => {
+  return users
+}
+
+// async stuff
+
+// add / transform props (spread)
